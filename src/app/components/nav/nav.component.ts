8b09d707bc580aba52b2042df443559dd4c77b13
emilio211204 <joseulcuango32@gmail.com>
2025-03-09 16:20:47 -0500

    nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -9,18 +9,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  autenticacion(): any {
-    if (typeof window !== 'undefined' && window.localStorage) {
-      const login = localStorage.getItem('login') === 'true';
-      const docente = localStorage.getItem('docente') === 'true';
-      const estudiantes = localStorage.getItem('estudiantes') === 'true';
-
-      return {
-        mostrarDocente: login && docente,
-        mostrarEstudiantes: login && estudiantes,
-        mostrarLogin: !login
-      };
-    }
-    return false;
+  autenticacion():boolean{
+    if(typeof window !== 'undefined' && window.localStorage) {
+    return localStorage.getItem('login') == 'true';
   }
+  return false;
+}
 }
